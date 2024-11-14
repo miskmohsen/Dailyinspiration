11aacaa24ebd371f53d4b85b7fb305684812e5a4
miskmohsen <127768011+miskmohsen@users.noreply.github.com>
2024-11-14 17:03:36 +0200

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+
+    }
+
+    dependencies {
+
+        classpath ("com.google.gms:google-services:4.3.15")
+
+    }
+}
+
+allprojects {
+
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
+
+
+
